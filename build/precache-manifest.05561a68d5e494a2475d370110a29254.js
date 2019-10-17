f54ab7fa5dfe78ca1742bc0f1909078f2e7a9b6d
Nagaraj Santhosh <santhosh.nagaraj@rakuten.com>
2019-10-17 21:37:28 +0530

    production build

---
+++ build/precache-manifest.05561a68d5e494a2475d370110a29254.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eefac577048590f78679e2937ef6fb8a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a24a1ee9788fcebb975",
+    "url": "/static/css/main.67368f67.chunk.css"
+  },
+  {
+    "revision": "a4da397ff774f05a44e1",
+    "url": "/static/js/2.6b43e86f.chunk.js"
+  },
+  {
+    "revision": "9a24a1ee9788fcebb975",
+    "url": "/static/js/main.6a298d28.chunk.js"
+  },
+  {
+    "revision": "c1d21c8f91d135426ae5",
+    "url": "/static/js/runtime-main.8407c2b3.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
